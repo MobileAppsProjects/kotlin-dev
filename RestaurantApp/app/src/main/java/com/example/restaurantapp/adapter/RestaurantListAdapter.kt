8684c65b2b182711b737a7ef58d3f62be46b61c3
taleslosada <ta.losada@uniandes.edu.co>
2024-04-26 11:33:30 -0500

    setOnClickListener

--- RestaurantApp/app/src/main/java/com/example/restaurantapp/adapter/RestaurantListAdapter.kt
+++ RestaurantApp/app/src/main/java/com/example/restaurantapp/adapter/RestaurantListAdapter.kt
@@ -7,66 +7,74 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.restaurantapp.R
 import com.example.restaurantapp.models.Hours
 import com.example.restaurantapp.models.RestaurantModel
-import com.example.restaurantapp.databinding.RecyclerRestaurantListRowBinding
 import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 
-class RestaurantListAdapter(val restaurantList: List<RestaurantModel?>?) : RecyclerView.Adapter<RestaurantListAdapter.MyViewHolder>() {
+class RestaurantListAdapter(val restaurantList: List<RestaurantModel?>?, val clickListener: RestaurantListClickListener): RecyclerView.Adapter<RestaurantListAdapter.MyViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val binding = RecyclerRestaurantListRowBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent,
-            false
-        )
-        return MyViewHolder(binding)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RestaurantListAdapter.MyViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.recycler_restaurant_list_row, parent, false)
+
+        return MyViewHolder(view)
     }
 
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val restaurant = restaurantList?.get(position)
-        restaurant?.let {
-            holder.bind(it)
+    override fun onBindViewHolder(holder: RestaurantListAdapter.MyViewHolder, position: Int) {
+        holder.bind(restaurantList?.get(position))
+        holder.itemView.setOnClickListener {
+            clickListener.onItemClick(restaurantList?.get(position)!!)
         }
     }
 
     override fun getItemCount(): Int {
-        return restaurantList?.size ?: 0
+        return restaurantList?.size!!
     }
 
-    class MyViewHolder(private val binding: RecyclerRestaurantListRowBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    inner class MyViewHolder(view: View): RecyclerView.ViewHolder(view)
+    {
+        val thumbImage: ImageView = view.findViewById(R.id.thumbImage)
+        val tvRestaurantName: TextView = view.findViewById(R.id.tvRestaurantName)
+        val tvRestaurantAddress: TextView = view.findViewById(R.id.tvRestaurantAddress)
+        val tvRestaurantHours: TextView = view.findViewById(R.id.tvRestaurantHours)
+
         fun bind(restaurantModel: RestaurantModel?) {
-            binding.tvRestaurantName.text = restaurantModel?.name
-            binding.tvRestaurantAddress.text = "Address: " + restaurantModel?.address
-            binding.tvRestaurantHours.text = "Today's Hours: " + getTodaysHours(restaurantModel?.hours!!)
+            tvRestaurantName.text = restaurantModel?.name
+            tvRestaurantAddress.text = "Address: "+restaurantModel?.address
+            tvRestaurantHours.text = "Today's Hours: " + getTodaysHours(restaurantModel?.hours!!)
 
-            Glide.with(binding.root)
-                .load(restaurantModel.image)
-                .into(binding.thumbImage)
-        }
-
-
-        private fun getTodaysHours(hours: Hours): String? {
-            val calendar: Calendar = Calendar.getInstance()
-            val date: Date = calendar.time
-            val day: String = SimpleDateFormat("EEEE", Locale.ENGLISH).format(date.time)
-            return when (day) {
-                "Sunday" -> hours.Sunday
-                "Monday" -> hours.Monday
-                "Tuesday" -> hours.Tuesday
-                "Wednesday" -> hours.Wednesday
-                "Thursday" -> hours.Thursday
-                "Friday" -> hours.Friday
-                "Saturday" -> hours.Saturday
-                else -> hours.Sunday
-            }
+            Glide.with(thumbImage)
+                .load(restaurantModel?.image)
+                .into(thumbImage)
         }
     }
+
+    private fun getTodaysHours(hours: Hours): String? {
+        val calendar: Calendar =  Calendar.getInstance()
+        val date: Date = calendar.time
+        val day : String = SimpleDateFormat("EEEE", Locale.ENGLISH).format(date.time)
+        return when(day) {
+            "Sunday" -> hours.Sunday
+            "Monday" -> hours.Monday
+            "Tuesday" -> hours.Tuesday
+            "Wednesday" -> hours.Wednesday
+            "Thursday" -> hours.Thursday
+            "Friday" -> hours.Friday
+            "Saturday" -> hours.Saturday
+            else -> hours.Sunday
+        }
+    }
+
+    interface RestaurantListClickListener {
+        fun onItemClick(restaurantModel: RestaurantModel)
+    }
+
 }
