fbef51bfd7e4a55e8c9020475d796857a769421e
taleslosada <ta.losada@uniandes.edu.co>
2024-04-26 19:04:06 -0500

    Menu screen fully implemented

--- RestaurantApp/app/src/main/java/com/example/restaurantapp/MainActivity.kt
+++ RestaurantApp/app/src/main/java/com/example/restaurantapp/MainActivity.kt
@@ -1,56 +1,59 @@
 package com.example.restaurantapp
 
-import android.os.Bundle
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.restaurantapp.adapter.RestaurantListAdapter
 import com.example.restaurantapp.models.RestaurantModel
 import com.google.gson.Gson
-import java.io.BufferedReader
-import java.io.InputStream
-import java.io.InputStreamReader
-import java.io.Reader
-import java.io.StringWriter
+import java.io.*
+import java.lang.Exception
 
-
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RestaurantListAdapter.RestaurantListClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setTitle("Restaurant List")
 
         val restaurantModel = getRestaurantData()
         initRecyclerView(restaurantModel)
     }
 
-    private fun initRecyclerView(restaurantList: List<RestaurantModel?>?){
+    private fun initRecyclerView(restaurantList: List<RestaurantModel?>?) {
         val recyclerViewRestaurant = findViewById<RecyclerView>(R.id.recyclerViewRestaurant)
         recyclerViewRestaurant.layoutManager = LinearLayoutManager(this)
-        val adapter = RestaurantListAdapter(restaurantList)
-        recyclerViewRestaurant.adapter = adapter
+        val adapter = RestaurantListAdapter(restaurantList, this)
+        recyclerViewRestaurant.adapter =adapter
     }
 
-
-
-    private fun getRestaurantData(): List<RestaurantModel?>?{
-        val InputStream: InputStream = resources.openRawResource(R.raw.restaurant)
-        val writer = StringWriter()
+    private fun getRestaurantData(): List<RestaurantModel?>? {
+        val inputStream: InputStream = resources.openRawResource(R.raw.restaurant)
+        val writer: Writer = StringWriter()
         val buffer = CharArray(1024)
-        try{
-           val reader: Reader = BufferedReader(InputStreamReader(InputStream, "UTF-8"))
-            var n: Int
-                    while (reader.read(buffer).also {n = it} != -1) {
-                        writer.write(buffer, 0, n)
-                    }
+        try {
+            val reader: Reader = BufferedReader(InputStreamReader(inputStream, "UTF-8"))
+            var n : Int
+            while (reader.read(buffer).also { n = it } != -1) {
+                writer.write(buffer, 0, n)
+
+            }
+
         }catch (e: Exception){}
         val jsonStr: String = writer.toString()
         val gson = Gson()
         val restaurantModel = gson.fromJson<Array<RestaurantModel>>(jsonStr, Array<RestaurantModel>::class.java).toList()
-        return restaurantModel;
+
+        return restaurantModel
+    }
+
+    override fun onItemClick(restaurantModel: RestaurantModel) {
+        val intent = Intent(this@MainActivity, RestaurantMenuActivity::class.java)
+        intent.putExtra("RestaurantModel", restaurantModel)
+        startActivity(intent)
     }
 }
---
+++ RestaurantApp/app/src/main/java/com/example/restaurantapp/RestaurantMenuActivity.kt
@@ -0,0 +1,117 @@
+package com.example.restaurantapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.restaurantapp.adapter.MenuListAdapter
+import com.example.restaurantapp.databinding.ActivityRestaurantMenuBinding
+import com.example.restaurantapp.models.Menus
+import com.example.restaurantapp.models.RestaurantModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class RestaurantMenuActivity : AppCompatActivity(), MenuListAdapter.MenuListClickListener {
+
+    private var itemsInTheCartList: MutableList<Menus?>? = null
+    private var totalItemInCartCount = 0
+    private var menuList: List<Menus?>? = null
+    private var menuListAdapter: MenuListAdapter? = null
+    private lateinit var binding: ActivityRestaurantMenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRestaurantMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val restaurantModel = intent?.getParcelableExtra<RestaurantModel>("RestaurantModel")
+
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setTitle(restaurantModel?.name)
+        actionBar?.setSubtitle(restaurantModel?.address)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        menuList = restaurantModel?.menus
+
+        CoroutineScope(Dispatchers.Main).launch {
+            initRecyclerView(menuList)
+            binding.checkoutButton.setOnClickListener {
+                if (itemsInTheCartList != null && itemsInTheCartList!!.size <= 0) {
+                    Toast.makeText(
+                        this@RestaurantMenuActivity,
+                        "",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    restaurantModel?.menus = itemsInTheCartList
+                    val intent =
+                        Intent(this@RestaurantMenuActivity, PlaceYourOrderActivity::class.java)
+                    intent.putExtra("RestaurantModel", restaurantModel)
+                    startActivityForResult(intent, 1000)
+                }
+            }
+        }
+
+    }
+    private suspend fun initRecyclerView(menus: List<Menus?>?) = withContext(Dispatchers.Main) {
+        binding.menuRecyclerVuew.layoutManager = GridLayoutManager(this@RestaurantMenuActivity, 2)
+        menuListAdapter = MenuListAdapter(menus, this@RestaurantMenuActivity)
+        binding.menuRecyclerVuew.adapter = menuListAdapter
+    }
+
+    override fun addToCartClickListener(menu: Menus) {
+        if(itemsInTheCartList == null) {
+            itemsInTheCartList = ArrayList()
+        }
+        itemsInTheCartList?.add(menu)
+        totalItemInCartCount = 0
+        for(menu in itemsInTheCartList!!) {
+            totalItemInCartCount = totalItemInCartCount + menu?.totalInCart!!
+        }
+        binding.checkoutButton.text = "Checkout (" + totalItemInCartCount +") Items"
+
+    }
+
+    override fun updateCartClickListener(menu: Menus) {
+        val index = itemsInTheCartList!!.indexOf(menu)
+        itemsInTheCartList?.removeAt(index)
+        itemsInTheCartList?.add(menu)
+        totalItemInCartCount = 0
+        for(menu in itemsInTheCartList!!) {
+            totalItemInCartCount = totalItemInCartCount + menu?.totalInCart!!
+        }
+        binding.checkoutButton.text = "Checkout (" + totalItemInCartCount +") Items"
+    }
+
+    override fun removeFromCartClickListener(menu: Menus) {
+        if(itemsInTheCartList!!.contains(menu)) {
+            itemsInTheCartList?.remove(menu)
+            totalItemInCartCount = 0
+            for(menu in itemsInTheCartList!!) {
+                totalItemInCartCount = totalItemInCartCount + menu?.totalInCart!!
+            }
+            binding.checkoutButton.text = "Checkout (" + totalItemInCartCount +") Items"
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            android.R.id.home -> finish()
+            else -> {}
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 1000 && resultCode == RESULT_OK) {
+            finish()
+        }
+    }
+}
